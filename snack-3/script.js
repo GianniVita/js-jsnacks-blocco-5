7c812a52f0e3d3501ba125b972bd4b9d70fe1978
giannivitale <giavitale78@gmail.com>
2025-10-22 15:21:33 +0200

    Third snack I used a for cicle to increment each number of the array and logged in the new array

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,11 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+const incrementedNumbers = []
+for (let i = 0; i < numbers.length; i++) {
+    const incrementedNumbers = numbers[i] + 1;
+    console.log(incrementedNumbers);
+    
+}
+
